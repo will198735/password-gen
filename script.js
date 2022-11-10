daf09583977076050038fd0c6db464e7414eb9da
narizon35 <recinoswilfredo@gmail.com>
2022-11-09 23:37:22 -0800

    modified JavaScript 2

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 // create 4 variables that store all our character types
 // put all characters in string, use .split method
+function randomInt(min, max) {
+  if (!max){
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 -rand) + rand*max)
+}
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+
 
 // to create a  generatePassword function that will return us our password
 
@@ -19,10 +31,44 @@ function generatePassword() {
     }
     var userWantsNumbers = window.confirm("would you like to include numbers in you password?")
     var userWantsSymbols = window.confirm("would you like to include symbols in you password?")
-    var userWantsLowercase = window.confirm("would you like to include letters in you password?")
-    var userWantsUppercase = window.confirm("would you like to include letters in you password?")
+    var userWantsLowercase = window.confirm("would you like to include lowercase letters in you password?")
+    var userWantsUppercase = window.confirm("would you like to include uppercase letters in you password?")
 
-  
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"< "(", ")", "-", "_", "=", "+"]
+  var lowercaseLIst = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaselist = []
+
+  var optionCart = []
+
+  for (var i = 0; i < lowercaseLIst.length; i++) {
+    uppercaselist[i] = lowercaseLIst[i].toUpperCase
+  }
+  if (userWantsNumbers === true) {
+    optionCart.push(numberList)
+  }
+  if (userWantsSymbols === true) {
+    optionCart.push(symbolList)
+
+  }
+  if (userWantsLowercase === true) {
+    optionCart.push(lowercaseLIst)
+  }
+  if (userWantsUppercase === true) {
+    optionCart.push(uppercaselist)
+  }
+
+  var generatePassword = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandomItem(optionCart)
+    var randomChar = getRandomItem(randomList)
+    generatePassword += randomChar
+
+    console.log(generatePassword)
+
+
+  }
 
     
 }
